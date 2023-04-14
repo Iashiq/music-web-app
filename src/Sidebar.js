1bdc93e5dd98ea9b035f19c1bcad73b35402695d
Ashiq Muhammad <muhammadashiqse10@gmail.com>
2023-04-15 01:59:31 +0200

    Added sidebar items and styled it
    Sidebar contains four items: home, search, library, and playlist options.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
 
 function Sidebar(){
         return(
           <div className="sidebar">
-            <h1>I am sidebar</h1>
+            <img className ="sidebar_logo" 
+            src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+            alt=""/>
+
+            <SidebarOption Icon={HomeIcon} title = "Home" />
+            <SidebarOption Icon={SearchIcon} title = "Search" />
+            <SidebarOption Icon={LibraryMusicIcon} title = "Your Library" />
+            
+            <br />
+            <strong className="playlist">PLAYLISTS</strong>
+            <hr />
           </div>
         );
 }
